ab55bebafc1faab00a92ca447548c45008c84b80
Tonkiyg <tonkiyg@gmail.com>
2024-03-25 12:57:24 +0200

    add cron and after-setup sync

--- apps/one-drive/src/connectors/share-point/drives.ts
+++ apps/one-drive/src/connectors/share-point/drives.ts
@@ -4,38 +4,6 @@ import { MicrosoftError } from '../../common/error';
 import type { MicrosoftPaginatedResponse } from '../../common/pagination';
 import { getNextSkipTokenFromNextLink } from '../../common/pagination';
 
-// 🚀 ~ getDriveId ~ response: {
-//   createdDateTime: '2024-02-18T00:25:20Z',
-//   description: '',
-//   id: 'b!4U30_TpBzkGAf3ZwnJkPzQCmcxAco59GoxgWJ7aOi6Wv5oyywfW_QqgcrdZH3rYt',
-//   lastModifiedDateTime: '2024-02-21T16:13:53Z',
-//   name: 'Documents',
-//   webUrl: 'https://testcomp633.sharepoint.com/sites/TestShared/Shared%20Documents',
-//   driveType: 'documentLibrary',
-//   createdBy: { user: { displayName: 'System Account' } },
-//   lastModifiedBy: {
-//     user: {
-//       email: 'HeorhiiTonkyi@TestComp633.onmicrosoft.com',
-//       id: 'b22da604-53e8-46bc-9fa4-4cad4305bae2',
-//       displayName: 'Heorhii Tonkyi'
-//     }
-//   },
-//   owner: {
-//     group: {
-//       email: 'TestShared@TestComp633.onmicrosoft.com',
-//       id: 'b3a6fd07-a9e1-4edc-8ce6-72d79cd7034d',
-//       displayName: 'TestShared Owners'
-//     }
-//   },
-//   quota: {
-//     deleted: 0,
-//     remaining: 27487783753686,
-//     state: 'normal',
-//     total: 27487790694400,
-//     used: 6940714
-//   }
-// }
-
 const driveSchema = z.object({
   id: z.string(),
 });
