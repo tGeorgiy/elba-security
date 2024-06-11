59c43cf3ab43410f936a6354c2e3a5d236def86d
Guillaume PROQUIN <guillaume.proquin@epitech.eu>
2024-06-11 17:52:21 +0200

    fix: migration

--- apps/sharepoint/drizzle/0000_wealthy_the_fury.sql
+++ apps/sharepoint/drizzle/0000_wealthy_the_fury.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS "sharepoint" (
 );
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "sharepoint" ADD CONSTRAINT "sharepoint_organisation_id_organisations_id_fk" FOREIGN KEY ("organisation_id") REFERENCES "public"."organisations"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "sharepoint" ADD CONSTRAINT "sharepoint_organisation_id_organisations_id_fk" FOREIGN KEY ("organisation_id") REFERENCES "organisations"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
